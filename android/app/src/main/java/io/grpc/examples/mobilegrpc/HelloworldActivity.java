e312e6d5e917ac7eba07a9abe2288da2ff46b3e9
pokerG <pokerfacehlg@gmail.com>
2015-03-13 18:00:42 +0800

    four style

--- android/app/src/main/java/io/grpc/examples/mobilegrpc/HelloworldActivity.java
+++ android/app/src/main/java/io/grpc/examples/mobilegrpc/HelloworldActivity.java
@@ -10,13 +10,18 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.RadioGroup;
+import android.widget.RadioButton;
 
 import java.util.concurrent.TimeUnit;
+import java.util.Iterator;
 
 import io.grpc.ChannelImpl;
-import io.grpc.examples.mobilegrpc.Mobile2Server.Reply;
-import io.grpc.examples.mobilegrpc.Mobile2Server.Request;
+import io.grpc.stub.StreamObserver;
+// import io.grpc.examples.mobilegrpc.Mobile2Server.Reply;
+import io.grpc.examples.mobilegrpc.Mobile2Server.*;
 import io.grpc.transport.okhttp.OkHttpChannelBuilder;
+import com.google.common.util.concurrent.SettableFuture;
 
 public class HelloworldActivity extends ActionBarActivity {
     private Button mSendButton;
@@ -24,7 +29,7 @@ public class HelloworldActivity extends ActionBarActivity {
     private EditText mPortEdit;
     private EditText mMessageEdit;
     private TextView mResultText;
-
+    private  RadioGroup  mRadioGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +39,7 @@ public class HelloworldActivity extends ActionBarActivity {
         mPortEdit = (EditText) findViewById(R.id.port_edit_text);
         mMessageEdit = (EditText) findViewById(R.id.message_edit_text);
         mResultText = (TextView) findViewById(R.id.grpc_response_text);
+        mRadioGroup = (RadioGroup)findViewById(R.id.rg);
     }
 
     public void sendMessage(View view) {
@@ -48,13 +54,21 @@ public class HelloworldActivity extends ActionBarActivity {
         private String mMessage;
         private int mPort;
         private ChannelImpl mChannel;
-
+        private int style;
+        private String re;
         @Override
         protected void onPreExecute() {
             mHost = mHostEdit.getText().toString();
             mMessage = mMessageEdit.getText().toString();
             String portStr = mPortEdit.getText().toString();
             mPort = TextUtils.isEmpty(portStr) ? 0 : Integer.valueOf(portStr);
+            for(int i = 0;i < mRadioGroup.getChildCount();i++){
+                RadioButton rd = (RadioButton)mRadioGroup.getChildAt(i);
+                if(rd.isChecked()){
+                    style = i + 1;
+                    break;
+                }
+            }  
             mResultText.setText("");
         }
 
@@ -66,11 +80,116 @@ public class HelloworldActivity extends ActionBarActivity {
             return reply.value;
         }
 
+        private String Spliti(ChannelImpl channel){
+            greeterGrpc.greeterBlockingStub stub = greeterGrpc.newBlockingStub(channel);
+            Request message = new Request();
+            message.value = mMessage;
+            re = "";
+            Iterator<sReply> replys = stub.spliti(message);
+            while(replys.hasNext()){
+                sReply reply = replys.next();
+                re += reply.id + ":" + reply.value + "\n";
+            }
+            return re;
+        }
+        private String Cat(ChannelImpl channel)throws Exception{
+            greeterGrpc.greeterStub stub = greeterGrpc.newStub(channel);
+            final SettableFuture<Void> finsishFuture = SettableFuture.create();
+            re = "";
+            StreamObserver<Reply> replyObserver = new StreamObserver<Reply>(){
+                @Override
+                public void onValue(Reply reply){
+                    re += reply.value;
+                }
+                @Override
+                public void onError(Throwable t){
+                    finsishFuture.setException(t);
+                }
+                @Override
+                public void onCompleted(){
+                    finsishFuture.set(null);
+                }
+            };
+            StreamObserver<sRequest> requestObserver = stub.cat(replyObserver);
+            try{
+                
+                String[] msgs = mMessage.split(";");
+                int i = 0;
+                for(String x: msgs){
+                    sRequest request = new sRequest();
+                    request.id = i++;
+                    request.value = x;
+                    requestObserver.onValue(request);
+                    if(finsishFuture.isDone()){
+                        break;
+                    } 
+                }
+                requestObserver.onCompleted();
+                finsishFuture.get();
+                return re;    
+            }catch(Exception e){
+                requestObserver.onError(e);
+                throw e;
+            }
+            
+        }
+        private String Transmit(ChannelImpl channel) throws Exception{
+            greeterGrpc.greeterStub stub = greeterGrpc.newStub(channel);
+            re = "";
+            final SettableFuture<Void> finsishFuture = SettableFuture.create();
+            StreamObserver<sRequest> requestObserver = stub.transmit(new StreamObserver<sReply>(){
+                @Override
+                public void onValue(sReply reply){
+                    re += reply.id + ":" + reply.value + "\n";
+                }
+                @Override
+                public void onError(Throwable t){
+                    finsishFuture.setException(t);
+                }
+                @Override
+                public void onCompleted(){
+                    finsishFuture.set(null);
+                }
+            });
+            try{
+                int i = 0;
+                String[] msgs = mMessage.split(";");
+                for(String x: msgs){
+                    sRequest request = new sRequest();
+                    request.id = i++;
+                    request.value = x;
+                    requestObserver.onValue(request);
+                    if(finsishFuture.isDone()){
+                        break;
+                    } 
+                }
+                requestObserver.onCompleted();
+                finsishFuture.get();
+                return re;
+            }catch(Exception t){
+                requestObserver.onError(t);
+                throw t;
+            }
+            
+            
+        }
+
         @Override
         protected String doInBackground(Void... nothing) {
             try {
                 mChannel = OkHttpChannelBuilder.forAddress(mHost, mPort).build();
-                return sayHello(mChannel);
+                switch(style){
+                    case 1:
+                        return sayHello(mChannel);
+                    case 2:
+                        return Spliti(mChannel);
+                    case 3:
+                        return Cat(mChannel);
+                    case 4:
+                        return Transmit(mChannel);
+                    default:
+                        return "Not have the function";
+                }
             } catch (Exception e) {
                 return "Failed... : " + e.getMessage();
             }
